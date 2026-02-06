9b47da7642bacccdc2ee991b81308e27335cd857
Javad <MrJavad.Email@gmail.com>
2026-02-06 23:09:47 +0330

    add UserCreate, UserUpdate and UserOut
    - change to src/schema/user.py

--- src/schema/user.py
+++ src/schema/user.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel
+from enums import Role
+from typing import Optional
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
-class User(BaseModel):
+class UserCreate(BaseModel):
     username: str
+    email: Optional[EmailStr] = None
     password: str
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str]
+    email: Optional[EmailStr] = None
+    password: Optional[str]
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: Optional[EmailStr]
+    role: Role
+
+    model_config = ConfigDict(from_attributes=True)
